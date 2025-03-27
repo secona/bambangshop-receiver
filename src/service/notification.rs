f19e7aa923bc1a74dfeb9308d74b51e8bbf0626c
secona <77039267+secona@users.noreply.github.com>
2025-03-27 23:03:14 +0700

    feat: Implement unsubscribe function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -52,10 +52,51 @@ impl NotificationService {
         }
     }
 
+    #[tokio::main]
+    async fn unsubscribe_request(product_type: String) -> Result<SubscriberRequest> {
+        let product_type_upper = product_type.to_uppercase();
+        let product_type_str = product_type_upper.as_str();
+        let notification_receiver_url = format!("{}/receive", APP_CONFIG.get_instance_root_url());
+
+        let request_url = format!("{}/notification/unsubscribe/{}?url={}",
+            APP_CONFIG.get_publisher_root_url(), product_type_str, notification_receiver_url);
+
+        let request = REQWEST_CLIENT
+            .post(request_url.clone())
+            .header("Content-Type", "application/json")
+            .header("Accept", "application/json")
+            .send()
+            .await;
+
+        log::warn_!("Sent unsubscribe request to: {}", request_url);
+
+        match request {
+            Ok(f) => match f.json::<SubscriberRequest>().await {
+                Ok(x) => Ok(x),
+                Err(_) => Err(compose_error_response(
+                    Status::NotAcceptable,
+                    String::from("Already unsubscribed to this topic."),
+                )),
+            }
+            Err(e) => Err(compose_error_response(
+                Status::NotFound,
+                e.to_string(),
+            ))
+        }
+    }
+
     pub fn subscribe(product_type: &str) -> Result<SubscriberRequest> {
         let product_type_clone = String::from(product_type);
 
         thread::spawn(move || Self::subscribe_request(product_type_clone))
+            .join()
+            .unwrap()
+    }
+
+    pub fn unsubscribe(product_type: &str) -> Result<SubscriberRequest> {
+        let product_type_clone = String::from(product_type);
+
+        thread::spawn(move || Self::unsubscribe_request(product_type_clone))
             .join()
             .unwrap()
     }
