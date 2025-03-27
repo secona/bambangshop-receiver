0c1b6a4921110a63ccdfc1e29e58b79c9b78e188
secona <77039267+secona@users.noreply.github.com>
2025-03-27 22:21:03 +0700

    feat: Create Notification database and Notification repository struct skeleton

---
+++ src/repository/notification.rs
@@ -0,0 +1,14 @@
+use std::sync::RwLock;
+
+use lazy_static::lazy_static;
+
+use crate::model::notification::Notification;
+
+lazy_static! {
+    static ref NOTIFICATIONS: RwLock<Vec<Notification>> = RwLock::new(Vec::new());
+}
+
+pub struct NotificationRepository;
+
+impl NotificationRepository {
+}
