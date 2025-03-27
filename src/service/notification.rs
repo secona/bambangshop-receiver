1b69fafe3adefca03bc74e28f2a62d671795399d
secona <77039267+secona@users.noreply.github.com>
2025-03-27 23:07:52 +0700

    feat: Implement receive_notification function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -100,4 +100,9 @@ impl NotificationService {
             .join()
             .unwrap()
     }
+
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_result = NotificationRepository::add(payload);
+        Ok(subscriber_result)
+    }
 }
